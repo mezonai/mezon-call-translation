0537f64ac4445b4febe9b01d4f8ebdf053caf146
QuineNguyen <huynh6255@gmail.com>
2026-07-23 17:14:48 +0700

    Use thread pool to manage notification tasks

--- Architect_MultiClient_Server/orchestrator_service/utils/retry_utils.py
+++ Architect_MultiClient_Server/orchestrator_service/utils/retry_utils.py
@@ -1,4 +1,3 @@
-
 from tenacity import RetryCallState
 from tenacity.wait import wait_base
 
@@ -11,6 +10,7 @@ class WaitCustomStrategy(wait_base):
     - Waits 70s after the second cycle (attempt 6)
     - Waits 80s after the third cycle (attempt 9), etc.
     """
+
     def __call__(self, retry_state: RetryCallState) -> float:
         attempt = retry_state.attempt_number
         if attempt % 3 != 0:
