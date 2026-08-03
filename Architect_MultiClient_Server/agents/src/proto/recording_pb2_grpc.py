9a60bf14852a98f5f7d9244e047b3ef36d01fd57
hoang.dohuy <hoang.dohuy@ncc.asia>
2026-08-03 16:30:58 +0700

    Feat: update proto

--- Architect_MultiClient_Server/agents/src/proto/recording_pb2_grpc.py
+++ Architect_MultiClient_Server/agents/src/proto/recording_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from . import recording_pb2 as recording__pb2
 
-GRPC_GENERATED_VERSION = '1.83.0'
+GRPC_GENERATED_VERSION = '1.81.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
